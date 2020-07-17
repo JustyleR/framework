97a5f85368da799f43d7d92bc00e4054b520428e
JustyleR <justyler1@gmail.com>
2020-07-17 19:54:25 +0300

    Added new stuff to the pagination library

--- libs/Pagination.php
+++ libs/Pagination.php
@@ -8,10 +8,12 @@ if (!defined('file_access')) {
 }
 
 // Pagination function
-// SQL -> SELECT * FROM news ORDER BY new_id
-// It will return array:
-// 0 -> pages , 1 -> query result
-function pagination($conn, $sql, $limit = 5) {
+  // $conn => SQL Connection
+  // $sql => SQL Query
+  // $limit => Limit for results per page
+  // $pages => How much pages to show on the pagination links
+// It will return an array with 2 values (0 => pagination , 1 => sql results)
+function pagination($conn, $sql, $limit = 5, $p_pages = 5) {
     $query = query($conn, $sql);
     if (num_rows($query) > 0) {
         $array = array();
@@ -61,7 +63,17 @@ function pagination($conn, $sql, $limit = 5) {
             $nextPage = $result;
         }
 
-        $pagination = array('prev' => $prevPage, 'next' => $nextPage, 'max' => $result);
+        $prevPages = [];
+        $nextPages = [];
+        for($i = max(1, $cpage - $p_pages); $i <= min($cpage + $p_pages, $result); $i++) {
+          if($i < $cpage) {
+            $prevPages[] = $i;
+          } else if($i > $cpage) {
+            $nextPages[] = $i;
+          }
+        }
+
+        $pagination = array('prev' => $prevPage, 'next' => $nextPage, 'max' => $result, 'prevPages' => $prevPages, 'nextPages' => $nextPages, 'cPage' => $cpage);
 
         return array($pagination, $array);
     } else {
